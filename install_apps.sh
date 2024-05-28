c5cf705bfdc2691d5b835ae7a297336cff77fa8d
ishay320 <ishay320@hotmail.com>
2024-05-28 13:02:48 +0300

    feat(fzf): updated the installation and update of fzf

--- install_apps.sh
+++ install_apps.sh
@@ -6,5 +6,9 @@ chmod u+x nvim.appimage
 sudo mv ./nvim.appimage /usr/local/bin/nvim
 
 # fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+if [ -f "$HOME/.fzf" ]; then
+    cd ~/.fzf && git pull && ./install
+else
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+fi
